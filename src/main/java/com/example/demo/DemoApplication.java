2f08e000f5ab603e5cce4bcd00bda3daf8989b03
Sai Hegde <shegde@lnrmacprolh02.myfamily.int>
2018-12-26 13:49:32 -0800

    app listens on multiple ports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import org.springframework.boot.web.server.WebServerFactory;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +25,23 @@ public class DemoApplication {
 	@Bean
 	public ServletRegistrationBean thriftServletRegistrationBean(){
     return new ServletRegistrationBean(new ThriftServlet(),"/thrift");
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer containerCustomizer() {
+	    return new WebServerFactoryCustomizer() {
+	        @Override
+	        public void customize(WebServerFactory webServerFactory) {
+	            if (webServerFactory instanceof TomcatServletWebServerFactory) {
+	                TomcatServletWebServerFactory tomcatServletWebServerFactory =
+	                        (TomcatServletWebServerFactory) webServerFactory;
+
+	                Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+	                connector.setPort(9043);
+	                tomcatServletWebServerFactory.addAdditionalTomcatConnectors(connector);
+	            }
+	        }
+	    };
 	}
 
 }
